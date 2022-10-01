7407f09e9e071b0d0c885972e831e44a01c5b5e6
mrlagcr (brian) <114592145+mrlagcr@users.noreply.github.com>
2022-10-01 00:11:15 -0600

    Add files via upload

---
+++ Tipos de datos Intro python.py
@@ -0,0 +1,28 @@
+# hola mundo
+# Video de youtube de mouredev
+print ("Hola python")
+
+
+"""
+Este es un
+comentario de
+varias lineas
+"""
+
+#Hay que tener cuidado con el tipo de datos (tomarlo en cuenta)
+#Tipicos datos son las cadenas de texto
+
+"""
+Integer: es para numeros enteros
+Float: es para numeros decimales
+Complex: es para numeros complejos
+"""
+
+#type sirve para imprimir el tipo de dato
+
+print (type ("Hola python")) #Tipo "Str"
+print (type(6)) #tipo "int"
+print (type(1.5)) #tipo float
+print (type(True)) # tipo "bool"
+print (type(3 + 3j)) # tipo "Complex"
+
